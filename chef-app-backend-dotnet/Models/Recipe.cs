1e4b8e69402105579237a4efd88b31fbe021c623
Daniel Potter <daniel.potter@onealphatech.com>
2022-03-13 13:48:16 +1100

    Inititialised the models and controllers folders and files.

---
+++ chef-app-backend-dotnet/Models/Recipe.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace chef_app_backend_dotnet.Models;
+
+public class Recipe
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public List<string>? Ingredients { get; set; }
+    public List<string>? Steps { get; set; }
+    [DataType(DataType.Date)]
+    public DateTime LastUpdated { get; set; }
+}
